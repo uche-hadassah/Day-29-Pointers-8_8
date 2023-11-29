18283e7a6497e5a7a6264049e53bc9629f50fe05
Hadassah Uche <uhadassah1@gmail.com>
2023-11-29 10:39:13 +0200

    Implemented the main

--- UcheHadassah8_8/HadassahUche8_8.cpp
+++ UcheHadassah8_8/HadassahUche8_8.cpp
@@ -1,2 +1,17 @@
 /*Write a void function that uses pointers to delete a single character from the end of a C-string. 
 The function takes as parameter the C-string*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+void removeChar(char*);//Function Prototype
+int main()
+{
+	char Str[100];
+	cout << "Enter a string:";
+	cin.getline(Str, 100);
+	removeChar(Str);
+	cout << "The new string is:" << Str;
+}
+void removeChar(char*)
+{
+}
